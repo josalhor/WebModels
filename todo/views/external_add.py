a684b4abc446b9e07e32a605cf1da8453fb2164a
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2021-04-11 11:14:34 +0200

    Work in progress emaill

--- todo/views/external_add.py
+++ todo/views/external_add.py
@@ -39,6 +39,8 @@ def external_add(request) -> HttpResponse:
         form = AddExternalTaskForm(request.POST)
         form_book = AddBookForm(request.POST)
 
+        print(form_book.is_valid())
+        print(form_book.errors)
         if form.is_valid() and form_book.is_valid():
             current_site = Site.objects.get_current()
             User = get_user_model()
@@ -61,30 +63,36 @@ def external_add(request) -> HttpResponse:
 
             
             messages.success(
-                request, "Your trouble ticket has been submitted. We'll get back to you soon."
+                request, "El teu llibre s'ha enviat. Ens possarem amb contacte amb tu en aviat."
             )
-            return redirect(defaults("TODO_PUBLIC_SUBMIT_REDIRECT"))
+
 
             # Send email to assignee if we have one
-            if task.assigned_to:
-                email_subject = render_to_string(
-                    "todo/email/assigned_subject.txt", {"task": task.title}
+            chief_editors = Editor.objects.filter(chief=True).all()
+            mails = [e.user.email for e in chief_editors]
+
+            email_subject = render_to_string(
+                "todo/email/assigned_subject.txt", {"title": book.name}
+            )
+            email_body = render_to_string(
+                "todo/email/assigned_body.txt", {"site": current_site, "book": book, "from_name": user_info.full_name}
+            )
+            try:
+                sent = send_mail(
+                    email_subject,
+                    email_body,
+                    None,
+                    mails,
+                    fail_silently=False,
                 )
-                email_body = render_to_string(
-                    "todo/email/assigned_body.txt", {"task": task, "site": current_site}
+                assert sent == len(mails)
+                print(sent)
+            except ConnectionRefusedError:
+                messages.warning(
+                    request, "Book saved but mail not sent. Contact your administrator."
                 )
-                try:
-                    send_mail(
-                        email_subject,
-                        email_body,
-                        task.created_by.email,
-                        [task.assigned_to.email],
-                        fail_silently=False,
-                    )
-                except ConnectionRefusedError:
-                    messages.warning(
-                        request, "Task saved but mail not sent. Contact your administrator."
-                    )
+            return redirect(defaults("TODO_PUBLIC_SUBMIT_REDIRECT"))
+
 
 
     else:
