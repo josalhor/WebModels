de5faabdd186b582e94d106ca0a940781b4add2a
Anna Torres Tuca <annattmail@gmail.com>
2021-05-21 23:13:33 +0200

    Show published books on main page

--- project/urls.py
+++ project/urls.py
@@ -5,11 +5,12 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.views.generic import TemplateView
 from .signup import signup
+from todo.views import book_catalogue
 
 urlpatterns = (
     [
-        path("", TemplateView.as_view(template_name="main.html"), name="main"),
-        path("init", TemplateView.as_view(template_name="main.html"), name="main"),
+        path("", book_catalogue, name="main"),
+        path("init", book_catalogue, name="main"),
         path("home", TemplateView.as_view(template_name="home.html"), name="home"),
         path("login", auth_views.LoginView.as_view(), name="login"),
         path("logout", auth_views.LogoutView.as_view(), name="logout"),
--- todo/views/book_catalogue.py
+++ todo/views/book_catalogue.py
@@ -4,14 +4,16 @@ import os
 import bleach
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from project import templates
 
 from todo.models import Book, PublishedBook
 
 def book_catalogue(request) -> HttpResponse:
-    books = PublishedBook.objects
+    books = PublishedBook.objects.all()
 
     context = {
         "published_books": books,
     }
 
-    return render(request, "project/templates/main.html", context)
+
+    return render(request, "todo/main.html", context)
